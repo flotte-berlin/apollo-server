449b6bd9ac6d8f56bc53c9821008229d014074bc
trivernis <trivernis@protonmail.com>
2020-09-14 15:45:28 +0200

    Fix database entity settings
    Signed-off-by: trivernis <trivernis@protonmail.com>

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { createConnection } from 'typeorm';
 import { UserServerAPI } from './datasources/userserver/userserviceAPI';
 import express from 'express';
 import { requiredPermissions } from './datasources/userserver/permission';
+import { CargoBike } from './model/CargoBike';
 
 require('dotenv').config();
 
@@ -39,7 +40,12 @@ async function authenticate (req: any, res: any, next: any) {
 
 createConnection({
     type: 'postgres',
-    url: process.env.POSTGRES_CONNECTION_URL
+    url: process.env.POSTGRES_CONNECTION_URL,
+    entities: [
+        CargoBike
+    ],
+    synchronize: true,
+    logging: false
 }).then(async () => {
     console.log('connected to db');
 }).catch(error => console.log(error));
